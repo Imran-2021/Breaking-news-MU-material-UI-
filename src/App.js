aa64cc5685895d36ac6edd4d70b27958dd81720a
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-09-12 18:36:06 +0600

    using material ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import React, { useEffect, useState } from 'react'
+import News from './Components/News';
+
 function App() {
+  const [articles , setArticles]=useState([]);
+    useEffect(() => {
+        fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-08-12&sortBy=publishedAt&apiKey=061db73a650d4c92a03a8db9c32816b9")
+        .then(req=>req.json())
+        .then(data=>setArticles(data.articles))
+    }, [])
   return (
-    <div style={{padding:"10px", margin:"10px"}}>
-        <Button variant="contained">Default</Button>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        Secondary
-      </Button>
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button variant="contained" color="primary" href="#contained-buttons">
-        Link
-      </Button>
+    <div className="clas" style={{padding:"10px", margin:"10px"}}>
+      {
+          articles.map(x=> <News news={x}/>)
+      }
     </div>
   );
 }
---
+++ src/Components/News.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+const News = (props) => {
+    const {author,content,description,publishedAt}= props.news
+    return (
+        <div >
+            <div>
+            <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="240"
+          image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {author}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {content}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {publishedAt}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+            </div>
+        </div>
+    );
+};
+
+export default News;
